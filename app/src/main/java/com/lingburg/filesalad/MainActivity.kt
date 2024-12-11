048648912a16f851485c97900fb300024d3787ba
Igor Nechaev <i.nechaev@lipt-soft.ru>
2024-12-11 12:02:07 +0300

    theme setup

--- app/src/main/java/com/lingburg/filesalad/MainActivity.kt
+++ app/src/main/java/com/lingburg/filesalad/MainActivity.kt
@@ -11,15 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.lingburg.filesalad.ui.theme.FileSaladTheme
+import com.lingburg.filesalad.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FileSaladTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            AppTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    FileSaladTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
